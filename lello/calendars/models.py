8958b2caa833171c8fa4d95050b26245657fdc0c
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-16 18:30:36 -0600

    boards model to check

--- lello/calendars/models.py
+++ lello/calendars/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 
-# Create your models here.
 
-# Calendar
-    # board
+class Calendar(models.Model):
+    class Calendar(models.Model):
+        board = models.ForeignKey(
+           'boards.Board',
+           null = False,
+           blank = False,
+           on_delete = models.CASCADE 
+        )
 
-# Event
-    # calendar
-    # date
-    # title
-    # description
-    # url
+        def __str__(self):
+            return self.board
+        
+    class Event(models.Model):
+        calendar = models.ForeignKey(
+            'calendars.Calendar',
+            null = False,
+            blank = False,
+            on_delete = models.CASCADE
+        )
+        date = models.DateField(
+            null = False
+        )
+        title = models.CharField(
+            max_length = 50,
+            null = False,
+            blank = False
+        )
+        description = models.CharField(
+            maxlength = 200,
+            null = True, 
+            blank = True
+        )
+        # TODO: Revisar el tipo de datp
+        url = models.CharField(
+            max_length = 200,
+            null = False,
+            blank = False
+        )
+
+        def __str__(self):
+            return self.title
