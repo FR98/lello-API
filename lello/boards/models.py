426a8dcb364ea489554099c4767b5dbddeaf5d8b
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-16 18:58:30 -0600

    boards model to check

--- lello/boards/models.py
+++ lello/boards/models.py
@@ -1,37 +1,131 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    class Board(models.Model):
+        name =  models.CharField(
+            max_length = 20,
+            null = False, 
+            blank = False
+        )
+        descriptiom = models.CharField(
+            max_length = 200,
+            null = True,
+            blank = True
+        )
+        owner = models.ForeignKey(
+            'users.User',
+            null = False,
+            blank = False,
+            on_delete = models.CASCADE
+        )
+        # TODO: Revisar, foreignkey??
+        privacy = models.IntegerField()
 
-# Board
-    # name 
-    # description
-    # owner
-    # privacy
+        def __str__(self):
+            return self.name
 
-# Member
-    # board
-    # user
-    # role
+    class Member(models.Model):
+        board = models.ForeignKey(
+            # TODO: REVISAR
+            'boards.Board',
+            null = False,
+            blank = False,
+            on_delete = models.SET_NULL
+        )
+        user = models.ForeignKey(
+            # TODO: REVISAR
+            'users.User',
+            null = False,
+            blank = False,
+            on_delete = models.SET_NULL
+        )
+        # TODO: REVISAR
+        role = models.ForeignKey(
+            'boards.Role',
+            null = False,
+            blank = False,
+            on_delete = models.SET_NULL
+        )
 
-# Role
-    # name
+        def __str__(self):
+            return self.user
 
-# List
-    # name
-    # hours ?
+    class Role(models.Model):
+        name = models.CharField(
+            max_length = 50,
+            null = False,
+            blank = False
+        )
 
-# Card
-    # name
-    # number
-    # description
-    # hours ?
-    # checklist
-    # deadline
-    # label
+        def __str__(self):
+            return self.name
 
-# Label
-    # name
+    class List(models.Model):
+        name = models.CharField(
+            max_length = 20,
+            null = False,
+            blank = False
+        )
+        # hours ?
 
-# Assigned m2m
-    # card
-    # user
+    class Card(models.Model):
+        name = models.CharField(
+            max_length = 20,
+            null = False,
+            blank = False
+        )
+        number = models.IntegerField()
+        description = models.CharField(
+            max_length = 200,
+            null = True,
+            blank = True
+        )
+        # hours ?
+        checklist = models.ForeignKey(
+            # TODO: REVISAR
+            'checklists.Checklist',
+            null = False,
+            blank = False,
+            on_delete = models.CASCADE
+        )
+        deadline = models.DateField(
+            null = True,
+            blank = True
+        )
+        label = models.ForeignKey(
+            'boards.Label',
+            null = False,
+            blank = False,
+            on_delete = models.SET_NULL
+        )
+
+        def __str__(self):
+            return self.number + self.name
+        
+    class Label(models.Model):
+        name = models.CharField(
+            max_length = 20,
+            null = False,
+            blank = False
+        )
+
+        def __str__(self):
+            return self.name
+
+    # m2m
+    class Assigned(models.Model):
+        card = models.ForeignKey(
+            'boards.Card',
+            null = False,
+            blank = False,
+            on_delete = models.CASCADE
+        )
+        user = models.ForeignKey(
+            'users.User',
+            null = False,
+            blank = False,
+            on_delete = models.CASCADE
+        )
+
+        def __str__(self):
+            return self.card + self.user
