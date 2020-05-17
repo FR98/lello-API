a32d699e11a3e276d9f08d2f7aaab1c7535a4435
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-16 18:03:56 -0600

    users model to check

--- lello/users/models.py
+++ lello/users/models.py
@@ -1,20 +1,69 @@
 from django.db import models
 
-# Create your models here.
 
-# UserDetail
-    # user 
-    # gender
-    # phone
-    # birthdate
+class User(models.Model):
+    class UserDetail(models.Model):
+        user = models.CharField(
+            max_length = 50, 
+            null = False, 
+            blank = False
+            )
+        gender = models.CharField(
+            max_length = 10,
+            null=True,
+            blank=True
+        )
+        phone = models.CharField(
+            max_length = 8,
+            null = True,
+            blank = True
+        )
+        birthdate = models.DateField(
+            null = False
+        )
 
-# Team
-    # name
+        def __str__(self):
+            return self.user
 
-# UserTeam
-    # team
-    # user
-    # role
+    class Team(models.Model):
+        name = models.CharField(
+            max_length = 50,
+            null = False,
+            blank = False
+        )
 
-# Role
-    # name
+        def __str__(self):
+            return self.name
+
+    class UserTeam(models.Model):
+        team = models.ForeignKey(
+            # TODO: Revisar foreign key
+            'users.Team',
+            null = False,
+            blank = False
+        )
+        user = models.ForeignKey(
+            # TODO: Revisar foreign key
+            'users.User',
+            null = False,
+            blank = False
+        )
+        role = models.ForeignKey(
+            # TODO: Revisar foreign key
+            'users.Role',
+            null = False,
+            blank = False
+        )
+
+        def __str__(self):
+            return self.team
+
+    class Role(models.Model):
+        name = models.CharField(
+            max_length = 20,
+            null = False,
+            blank = False
+        )
+
+        def __str__(self):
+            return self.name
