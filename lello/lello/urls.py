09fec31f041cc53be9fcb4f7ec3c7d9965e117a1
FR98 <ros18676@uvg.edu.gt>
2020-05-17 20:38:06 -0600

    Change token urls

--- lello/lello/urls.py
+++ lello/lello/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
     url(r'^api/', include(router.urls)),
-    url(r'^api-token/auth/', obtain_jwt_token),
-    url(r'^api-token/refresh/', refresh_jwt_token),
-    url(r'^api-token/verify/', verify_jwt_token),
+    url(r'^api/token-auth/', obtain_jwt_token),
+    url(r'^api/token-refresh/', refresh_jwt_token),
+    url(r'^api/token-verify/', verify_jwt_token),
 ]
